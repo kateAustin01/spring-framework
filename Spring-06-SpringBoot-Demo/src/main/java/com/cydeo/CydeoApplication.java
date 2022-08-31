ac295a41a295fe099c996af29e90b9d32474067e
aissakhova@gmail.com <aktoty@live.com>
2022-08-31 10:22:08 -0500

    hmtl age added

---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -0,0 +1,30 @@
+package com.cydeo;
+
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentsService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class CydeoApplication {
+
+	public static void main(String[] args) {
+
+
+		Comment comment = new Comment();
+		comment.setAuthor("Johnson");
+		comment.setText("Spring framework");
+
+		ApplicationContext context = SpringApplication.run(CydeoApplication.class, args);
+
+		CommentsService cs = context.getBean(CommentsService.class);
+		cs.publishComment(comment);
+
+
+
+
+	}
+
+}
